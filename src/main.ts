28a6e5c81079e2090e221f0499056e7353b022ca
MarianoHernandezEscobal <mh202428@fi365.ort.edu.uy>
2026-04-11 13:13:07 -0300

    change port

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    console.log('Aplicación iniciada', process.env.PORT);
-
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  console.log('Aplicación iniciada', process.env.PORT);
-  await app.listen(process.env.PORT ?? 3002);
+
+  const port = parseInt(process.env.PORT || '3002', 10);
+
+  await app.listen(port);
+
+  console.log(`🚀 Aplicación corriendo en puerto: ${port}`);
 }
 bootstrap();
-
